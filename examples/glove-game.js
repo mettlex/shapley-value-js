1e4b3de8a0744884f289eef174afe8d0a4a9153a
Mettle X <56449790+mettlex@users.noreply.github.com>
2021-08-22 00:02:37 +0600

    add example

---
+++ examples/glove-game.js
@@ -0,0 +1,35 @@
+// const { shapley } = require("shapley");
+const { shapley } = require("../");
+
+// player 1, 2 & 3
+const N = [1, 2, 3];
+
+/** @param {Set<number>} S */
+const v = (S) => {
+  const A = Array.from(S);
+
+  // players 1 and 2 have right-hand gloves and
+  // player 3 has a left-hand glove
+  const first = [1, 3];
+  const second = [2, 3];
+  const third = [1, 2, 3];
+
+  const U = [first, second, third];
+
+  let val = 0;
+
+  for (const col of U) {
+    if (!col.find((el) => !A.includes(el))) {
+      val = 1;
+      break;
+    }
+  }
+
+  return val;
+};
+
+const svP1 = shapley(N, v)(1);
+const svP2 = shapley(N, v)(2);
+const svP3 = shapley(N, v)(3);
+
+console.log(svP1 === 1 / 6, svP2 === 1 / 6, svP3 === 2 / 3);
